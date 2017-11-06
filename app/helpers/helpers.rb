157e2d9a8160cfcca5ee1c2b8b40d24f2e23a3b2
Emily Morgan <emorgan05@gmail.com>
2017-11-06 17:12:51 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -4,7 +4,7 @@ class Helpers
 
   def self.current_user(session)
     binding.pry
-    user = User.find_by(user_id: session[:user_id])
+    user = User.find(session[:user_id])
   end
 
   def self.is_logged_in?(session)
