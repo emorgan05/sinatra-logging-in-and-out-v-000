94d834c9f06a730e76eed224af0737f0df5fb1ab
Emily Morgan <emorgan05@gmail.com>
2017-11-06 18:23:06 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 require_relative '../../config/environment'
-class ApplicationController < Sinatra::Base
-  helpers Helpers
-  
+class ApplicationController < Sinatra::Base  
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
     enable :sessions unless test?
