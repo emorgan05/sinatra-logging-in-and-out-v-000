11a51655a559b386f3f11b0b88c27aac21c022fa
Emily Morgan <emorgan05@gmail.com>
2017-11-06 18:34:50 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 require_relative '../../config/environment'
-class ApplicationController < Sinatra::Base  
+class ApplicationController < Sinatra::Base
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
     enable :sessions unless test?
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     if Helpers.is_logged_in?(session)
-      @user = User.find(session[:user_id])
+      @user = Helpers.current_user(session)
 
       erb :account
     else
